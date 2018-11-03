01a0f1c0455ebcaf25d85b0d5f9951d68b2ebb6d
zhufeng <>
2018-11-03 12:01:30 +0800

    第二次提交代码，导入项目结构

--- mitugou/mitugou/ViewController.m
+++ mitugou/mitugou/ViewController.m
@@ -1,10 +1,7 @@
-//
 //  ViewController.m
 //  mitugou
-//
 //  Created by zhufeng on 2018/11/3.
 //  Copyright © 2018年 zhufeng. All rights reserved.
-//
 
 #import "ViewController.h"
 
@@ -16,14 +13,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-
 @end
